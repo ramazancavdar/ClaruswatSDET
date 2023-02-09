46c48222bfcdd65f00e5fec393396c84a6712ba4
ramazancavdar <124646914+ramazancavdar@users.noreply.github.com>
2023-02-08 21:12:32 -0500

    Initial commit: I created SDET2 Project and HelloWorld class.

---
+++ src/week1/HelloWorld.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+        System.out.print("Welcome to Clarusway SDET Course: ");
+        System.out.println("This line is starting with the previous line");
+    }
+}
