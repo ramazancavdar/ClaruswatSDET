e573a7684d19c634675f910a2aa9bfe33b89eefb
ramazancavdar <124646914+ramazancavdar@users.noreply.github.com>
2023-02-08 21:31:59 -0500

    i tried something myself

--- src/week1/HelloWorld.java
+++ src/week1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World!!!");
         System.out.print("Welcome to Clarusway SDET Course: ");
         System.out.println("This line is starting with the previous line");
+        System.out.println("This is a new line");
     }
 }
