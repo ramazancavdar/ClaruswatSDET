8b52a29e8daa65b14311b0e04412bba4dacdc5a5
ramazancavdar <124646914+ramazancavdar@users.noreply.github.com>
2023-02-08 22:44:55 -0500

    I added Data Types class.

---
+++ src/Comments.java
@@ -0,0 +1,18 @@
+public class Comments {
+    //   This is the first type of comment starts with //
+       // This is comment
+
+    /* I can write multiple comments in here.
+    This is my first line
+    and This is my second line
+    and this is my third line
+    */
+
+    /**
+     * This is another comment with multi lines
+     * this is second line
+     * comment 3rd line
+     **/
+
+
+    }
---
+++ src/DataTypes.java
@@ -0,0 +1,27 @@
+
+
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //Variables declaration as below
+        int age;
+        char initial;
+
+        //Assigning the values to variables as below:
+        age = 31;
+        initial = 'M';
+
+        int age1, height, distance;
+        age1 = 29;
+        height = 180;
+        distance = 64;
+
+        int age2 = 34, height1 = 145, distance1 = 41;
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(distance);
+
+        System.out.println(initial);
+    }
+}
